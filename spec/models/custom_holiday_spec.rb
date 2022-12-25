246cbe589dbe2d27037effa91f61c07f4ad01cd2
seita1996 <taha.seita@gmail.com>
2022-12-25 21:44:04 +0900

    Fixed warning points in rubocop

--- spec/models/custom_holiday_spec.rb
+++ spec/models/custom_holiday_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'Model CustomHoliday', type: :model do
   describe 'method include?' do
-    let!(:custom_holiday_1) { FactoryBot.create(:custom_holiday, date: Date.today - 1) }
-    let!(:custom_holiday_2) { FactoryBot.create(:custom_holiday, date: Date.today - 2) }
+    let!(:custom_holiday1) { FactoryBot.create(:custom_holiday, date: Date.today - 1) }
+    let!(:custom_holiday2) { FactoryBot.create(:custom_holiday, date: Date.today - 2) }
 
     context '引数の日付がcustom_holidayに登録されているとき' do
       it 'trueを返す' do
-        expect(CustomHoliday.include?(custom_holiday_1.date)).to eq true
+        expect(CustomHoliday.include?(custom_holiday1.date)).to eq true
       end
     end
 
