8c675374273f4c5a503dacd976f88401e8869594
seita1996 <taha.seita@gmail.com>
2023-01-01 16:24:04 +0900

    Add CustomHoliday's validations

--- app/models/custom_holiday.rb
+++ app/models/custom_holiday.rb
@@ -1,4 +1,7 @@
 class CustomHoliday < ApplicationRecord
+  validates :date, presence: true
+  validates :date, uniqueness: { case_sensitive: false }
+  validates :description, length: { maximum: 50 }
   def self.include?(date)
     return false if CustomHoliday.find_by(date: date).nil?
     true
--- spec/models/custom_holiday_spec.rb
+++ spec/models/custom_holiday_spec.rb
@@ -1,6 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe 'Model CustomHoliday', type: :model do
+  describe 'validations' do
+    context 'dateが空のとき' do
+      let(:custom_holiday) { CustomHoliday.new(date: '', description: 'test') }
+      it '登録に失敗する' do
+        expect(custom_holiday.save).to eq(false)
+      end
+    end
+    context 'dateが既に登録されているデータと重複しているとき' do
+      let!(:pre_saved_custom_holiday) { FactoryBot.create(:custom_holiday, date: '2023-01-01', description: 'test') }
+      let(:custom_holiday) { CustomHoliday.new(date: '2023-01-01', description: 'test') }
+      it '登録に失敗する' do
+        expect(custom_holiday.save).to eq(false)
+      end
+    end
+    context 'dateが不正な日付のとき' do
+      let(:custom_holiday) { CustomHoliday.new(date: '2023-13-32', description: 'test') }
+      it '登録に失敗する' do
+        expect(custom_holiday.save).to eq(false)
+      end
+    end
+    context 'dateが正しい日付のとき' do
+      let(:custom_holiday) { CustomHoliday.new(date: '2023-01-01', description: 'test') }
+      it '登録に成功する' do
+        expect(custom_holiday.save).to eq(true)
+      end
+    end
+    context 'descriptionが50文字のとき' do
+      let(:custom_holiday) { CustomHoliday.new(date: '2023-01-01', description: 'a' * 50) }
+      it '登録に成功する' do
+        expect(custom_holiday.save).to eq(true)
+      end
+    end
+    context 'descriptionが51文字のとき' do
+      let(:custom_holiday) { CustomHoliday.new(date: '2023-01-01', description: 'a' * 51) }
+      it '登録に失敗する' do
+        expect(custom_holiday.save).to eq(false)
+      end
+    end
+  end
   describe 'method include?' do
     let!(:custom_holiday1) { FactoryBot.create(:custom_holiday, date: Date.today - 1) }
     let!(:custom_holiday2) { FactoryBot.create(:custom_holiday, date: Date.today - 2) }
