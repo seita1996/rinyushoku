e809ec7e42df8a65c97f53ec7a6bf7c4d4d84da1
seita1996 <taha.seita@gmail.com>
2023-01-13 20:46:48 +0900

    Update CORS setting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    resource '/api/*',
+      headers: :any,
+      methods: :any
   end
 end
