ec892e416f5b46e53a1a2b0df273655301e6e2b4
seita1996 <taha.seita@gmail.com>
2023-01-18 20:31:28 +0900

    Change the color of the card on the schedule that has the first food item you eat

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,4 +1,10 @@
 module SchedulesHelper
+  def card_classes(schedule)
+    return 'shadow-lg border rounded px-6 py-4 mb-2 bg-red-50' if schedule.has_debut_food
+
+    'shadow-lg border rounded px-6 py-4 mb-2'
+  end
+
   JAPANESE_WDAY = %w[日 月 火 水 木 金 土].freeze
 
   def view_date(date)
