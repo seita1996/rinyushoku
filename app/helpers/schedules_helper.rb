f33bad23a498eb353ce222ed0ec0e4123473d979
seita1996 <taha.seita@gmail.com>
2023-01-18 15:09:55 +0900

    Corrected rubocop's point

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,5 +1,5 @@
 module SchedulesHelper
-  JAPANESE_WDAY = %w(日 月 火 水 木 金 土)
+  JAPANESE_WDAY = %w[日 月 火 水 木 金 土].freeze
 
   def view_date(date)
     date.strftime("%Y年%m月%d日（#{JAPANESE_WDAY[date.wday]}）")
