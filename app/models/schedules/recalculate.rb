aa0fb9e725d97c96e09abd290495db2c6810872d
seita1996 <taha.seita@gmail.com>
2023-01-28 10:36:52 +0900

    Refactoring recalculate logic

--- app/models/schedules/recalculate.rb
+++ app/models/schedules/recalculate.rb
@@ -1,39 +1,58 @@
 module Schedules
   class Recalculate
-    def self.call(day, start_date)
-      return false if day.empty? || start_date.empty?
+    class << self
+      ScheduleRecord = Struct.new(:date, :has_debut_food, :meal_id, keyword_init: true)
 
-      start_date = Date.parse(start_date)
+      def call(day, start_date)
+        return false if day.empty? || start_date.empty?
 
-      # テーブルの更新箇所以降のデータを全削除
-      Schedule.includes(:meal).where(meals: { day: day.. }).destroy_all
-
-      current_date = start_date - 1 # ループに入ってすぐ+1するため-1しておく
-      schedules = []
-      Meal.where(day: day..).each do |meal|
-        # 2回食以降は同日
-        current_date += 1 if meal.ordinal_number == 1
-        repeat_result = save_repeat_schedule(meal, current_date, schedules)
-        current_date = repeat_result[:current_date]
-        schedules = repeat_result[:schedules]
-        schedules << { date: current_date, has_debut_food: meal.has_debut_food, meal_id: meal.id }
+        start_date = Date.parse(start_date)
+        delete_schedules(day)
+        schedules = create_schedules(day, start_date)
+        Schedule.insert_all(schedules)
+        true
       end
-      Schedule.insert_all(schedules)
-      true
-    end
 
-    def self.save_repeat_schedule(meal, current_date, schedules)
-      # 初めて食べる食材があり、当日が日曜または祝日またはカスタム休日ならば
-      # 前日のScheduleを複製したあと日付をインクリメント（休日以外の日になるまで繰り返す）
-      while meal.has_debut_food && (current_date.wday.zero? || HolidayJp.holiday?(current_date) || CustomHoliday.include?(current_date))
-        schedules.filter { |schedule| schedule[:date] == current_date - 1 }.each do |schedule|
-          dup_schedule = schedule.dup
-          dup_schedule[:date] = current_date
-          schedules << dup_schedule
+      private
+
+      def create_schedules(day, start_date)
+        schedules = []
+        current_date = start_date - 1 # ループに入ってすぐ+1するため-1しておく
+        Meal.where(day: day..).each do |meal|
+          # 2回食以降は同日
+          current_date += 1 if meal.ordinal_number == 1
+
+          # 初めて食べる食材があり、当日が日曜または祝日またはカスタム休日ならば
+          # 前日のScheduleを複製したあと日付をインクリメント（休日以外の日になるまで繰り返す）
+          while meal.has_debut_food && holiday?(current_date)
+            dup_yesterday_schedules(current_date, schedules).each do |dup_schedule|
+              schedules << dup_schedule
+            end
+            current_date += 1
+          end
+          schedules << ScheduleRecord.new(date: current_date, has_debut_food: meal.has_debut_food, meal_id: meal.id)
         end
-        current_date += 1
+        schedules.map(&:to_h)
       end
-      { current_date:, schedules: }
+
+      def delete_schedules(first_day_to_delete)
+        # テーブルの更新箇所以降のデータを全削除
+        Schedule.includes(:meal).where(meals: { day: first_day_to_delete.. }).destroy_all
+      end
+
+      def holiday?(current_date)
+        current_date.wday.zero? || HolidayJp.holiday?(current_date) || CustomHoliday.include?(current_date)
+      end
+
+      def dup_yesterday_schedules(current_date, schedules)
+        result = []
+        schedules.filter { |schedule| schedule.date == current_date - 1 }.each do |schedule|
+          dup_schedule = schedule.dup
+          dup_schedule.date = current_date
+          result << dup_schedule
+        end
+        result
+      end
     end
   end
 end
