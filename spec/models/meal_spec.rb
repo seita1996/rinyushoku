b29311e2e9c6c70db2b9f33df64d0afb56e6c925
seita1996 <taha.seita@gmail.com>
2023-03-03 09:17:50 +0900

    Add meal.has_debut_food spec

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -52,4 +52,26 @@ RSpec.describe 'Model Meal', type: :model do
       end
     end
   end
+  describe 'method has_debut_food' do
+    let(:file_path) { file_fixture('rinyushoku_success.csv') }
+    before do
+      # 事前にテンプレートデータをインポートしておく
+      ImportMealTemplate.call(file_path: file_path)
+
+      # インポートしたテンプレートからスケジュールを登録
+      RecalculateSchedule.call(day: '1', start_date: '2023-01-16')
+    end
+    context '該当する食事に「初めて食べる食材」が1つでも含まれている場合' do
+      it 'trueを返す' do
+        day2_1st_meal = Meal.find_by(day: 2, ordinal_number: 1)
+        expect(day2_1st_meal.has_debut_food).to eq true
+      end
+    end
+    context '該当する食事に「初めて食べる食材」が含まれない場合' do
+      it 'falseを返す' do
+        day3_2nd_meal = Meal.find_by(day: 3, ordinal_number: 2)
+        expect(day3_2nd_meal.has_debut_food).to eq false
+      end
+    end
+  end
 end
