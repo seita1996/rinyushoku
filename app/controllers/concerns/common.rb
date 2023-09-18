2f624b2d3507f9d5f0ac040f75148f6c2af07091
seita1996 <taha.seita@gmail.com>
2023-09-18 20:35:10 +0900

    fix rubocop error

--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -8,6 +8,6 @@ module Common
   def call_api(url)
     uri = URI(url)
     res = Net::HTTP.get_response(uri)
-    return res.body if res.is_a?(Net::HTTPSuccess)
+    res.body if res.is_a?(Net::HTTPSuccess)
   end
 end
